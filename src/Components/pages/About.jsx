08a29fd69cdf1283c45cb06ad94b7c2ab942b52d
Firefox <tshankgg@gmail.com>
2025-02-11 19:53:37 -0600

    worked on about me section and added picture

--- src/Components/pages/About.jsx
+++ src/Components/pages/About.jsx
@@ -1,6 +1,10 @@
+import "../../App.css";
+import PortfolioPic from "../../assets/Portfolio Default.jpg";
+
 export default function About() {
   return (
-    <div>
+    <div className="about-container">
+      <img src={PortfolioPic} alt="Tanner Shank" className="about-image" />
       <h1>About Me</h1>
       <p>
         My name is Tanner Shank, I am an aspiring full stack web developer coming out of the University of Minnesota's bootcamp program!
